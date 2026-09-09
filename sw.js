9e30e7a84ac3f4ffcb183dc3a5e07f4959e40b2b
SrGrlk <teyyqueota@gmail.com>
2026-09-09 16:58:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /* ==========================================================================
-   CREATIO SERVICE WORKER — 100% BULLETPROOF OFFLINE ENGINE (v11)
+   CREATIO SERVICE WORKER — 100% BULLETPROOF OFFLINE ENGINE (v12)
    Resilient app-shell caching, instant offline navigation & dynamic cache.
    ========================================================================== */
 
-const CACHE_NAME = 'creatio-offline-v11';
+const CACHE_NAME = 'creatio-offline-v12';
 const CORE_ASSETS = [
   './',
   './index.html',
